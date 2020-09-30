ebdb5f9e0baeb8658ba643b434c3cdb64da8512f
Tomás R. Pita <tomasrpita@gmail.com>
2020-09-30 06:30:14 +0200

    agrega implementación de  InheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/pages/bloc/provider.dart';
 import 'package:formvalidation/src/pages/home_page.dart';
 import 'package:formvalidation/src/pages/login_page.dart';
 
@@ -7,14 +8,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Form Validation - Bloc',
-      initialRoute: 'login',
-      routes: {
-        'login': (BuildContext context) => LoginPage(),
-        'home': (BuildContext context) => HomePage(),
-      },
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Form Validation - Bloc',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'home': (BuildContext context) => HomePage(),
+        },
+      ),
     );
   }
 }
---
+++ lib/src/pages/bloc/provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/pages/bloc/login_bloc.dart';
+
+// Vas a ser mi in personalizado
+// Puede llamarse de diferente forma,
+// puede manejar varios bloc
+class Provider extends InheritedWidget {
+  final loginBloc = LoginBloc();
+
+  Provider({Key key, Widget child}) : super(key: key, child: child);
+
+  @override
+  // en la mayoria de los casos vamos a que querer que haga notificación
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static LoginBloc of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+  }
+}
